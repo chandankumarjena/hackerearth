55ddf162fc1de554685cd715dbc62e60b6e59677
chandankumarj <chandankumarj@192.168.0.102>
2019-08-03 23:02:35 +0530

    second staging

--- hackerearth/src/com/chandan/basicprogramming/io/PrimeNumber.java
+++ hackerearth/src/com/chandan/basicprogramming/io/PrimeNumber.java
@@ -1,10 +1,38 @@
 package com.chandan.basicprogramming.io;
+/* find all the prime number list till an input number N. 
+ * input number 9
+ * output result 2 3 5 7
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class PrimeNumber {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = Integer.parseInt(br.readLine());
+		for (int i = 2; i<= number ; i++) {
+			boolean status = checkPrime(i);
+			if(status) {
+				System.out.print(i + " ");
+			}
+		}		
+		br.close();
+	}
 
+	private static boolean checkPrime(int number) {
+		// TODO Auto-generated method stub
+		if (number <= 1) {
+			return false;
+		}
+		for (int i = 2; i<=Math.sqrt(number); i++) {
+			if(number%i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
