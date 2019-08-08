2e552362f92159cac63f1b0c5d31f486d58e01b9
chandankumarj <chandankumarj@100.95.207.125>
2019-08-08 16:06:50 +0530

    factorial

---
+++ hackerearth/src/com/chandan/basicprogramming/io/Factorial.java
@@ -0,0 +1,29 @@
+package com.chandan.basicprogramming.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Factorial {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = Integer.parseInt(br.readLine());
+		int fact = getFactorial(number);
+		System.out.print(fact);
+
+	}
+
+	private static int getFactorial(int number) {
+		// TODO Auto-generated method stub
+		int result = 1;
+		if (number >= 1 && number <=10) {
+			for (int i = 1; i<=number; i++) {
+				result = result * i;
+			}
+		}
+		return result;
+	}
+
+}
