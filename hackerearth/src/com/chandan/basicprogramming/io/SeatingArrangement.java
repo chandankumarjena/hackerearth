0a91e758a1d4139f0f9a2f16cc0eb872f0194a4e
chandankumarj <chandankumarj@192.168.0.104>
2019-08-09 21:09:43 +0530

    ok

--- hackerearth/src/com/chandan/basicprogramming/io/SeatingArrangement.java
+++ hackerearth/src/com/chandan/basicprogramming/io/SeatingArrangement.java
@@ -19,8 +19,6 @@ package com.chandan.basicprogramming.io;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SeatingArrangement {
 
