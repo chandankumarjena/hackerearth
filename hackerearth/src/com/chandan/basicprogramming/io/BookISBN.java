075ea96138f682b0a8f31d0600d858ac6bc7bd20
chandankumarj <chandankumarj@100.95.207.125>
2019-08-09 17:15:17 +0530

    BookISBN

---
+++ hackerearth/src/com/chandan/basicprogramming/io/BookISBN.java
@@ -0,0 +1,28 @@
+package com.chandan.basicprogramming.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BookISBN {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		char []array = bufferedReader.readLine().toCharArray();
+		if(array.length == 10) {
+			int total = 0;
+			for(int i = 0; i < 10; i++) {
+				int j = i + 1;
+				total = total + (j*Character.getNumericValue(array[i]));
+			}
+			if(total % 11 == 0) {
+				System.out.println("Legal ISBN");
+			} else {
+				System.out.println("Illegal ISBN");
+			}	
+		} else {
+			System.out.println("Illegal ISBN");
+		}
+ }
+}
