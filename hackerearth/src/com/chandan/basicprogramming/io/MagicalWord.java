b9ab60c81f1d46873e689f6eb7a507339c57ed7b
chandankumarj <chandankumarj@192.168.0.101>
2019-08-18 21:42:11 +0530

    ok

--- hackerearth/src/com/chandan/basicprogramming/io/MagicalWord.java
+++ hackerearth/src/com/chandan/basicprogramming/io/MagicalWord.java
@@ -8,7 +8,7 @@ public class MagicalWord {
 	public static void main(String[] args)  throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		int p[] = {67,71,73,79,83,89,97,101,103,107,109,113};
+		int container[] = {67,71,73,79,83,89,97,101,103,107,109,113};
 		int testCases = Integer.parseInt(bufferedReader.readLine());
         int it = 0;
         int pd = 0;
@@ -26,21 +26,21 @@ public class MagicalWord {
                  pd = 0;
                  nd = 0;
                 if(st.charAt(i)<=67){
-                    res += (char)67;
                 }
                 else if(st.charAt(i) >= 113) {
+                	res += (char)67;
                     res += (char)113;
                 }
                 else {
-                    while(st.charAt(i) >= p[it])
+                    while(st.charAt(i) >= container[it])
                         it++;
-                   pd = st.charAt(i)- p[it-1] ;
-                   nd = p[it] - st.charAt(i) ;
+                   pd = st.charAt(i)- container[it-1] ;
+                   nd = container[it] - st.charAt(i) ;
                    if(pd<=nd) {
-                       res += (char)p[it-1];
+                       res += (char)container[it-1];
                     }
                     else {
-                       res += (char)p[it];
+                       res += (char)container[it];
                     }
                 }
             }
