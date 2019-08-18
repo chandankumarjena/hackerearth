45b7f360f667c3c9c6c3e942c1318be7696fedfc
chandankumarj <chandankumarj@192.168.0.101>
2019-08-18 20:58:32 +0530

    Split Houses

---
+++ hackerearth/src/com/chandan/basicprogramming/io/SplitHouses.java
@@ -0,0 +1,31 @@
+package com.chandan.basicprogramming.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SplitHouses {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number=Integer.parseInt(br.readLine());
+        int i;
+        String str=br.readLine();
+        for(i=0;i<number-1;i++)
+        {
+        	if(str.charAt(i)=='H' && str.charAt(i+1)=='H')
+        		break;
+        }
+        if(i==number-1)
+        {
+        	    System.out.println("YES");
+                System.out.print(str.replace('.','B'));
+        }
+        else {
+            System.out.println("NO");
+        }
+
+	}
+
+}
